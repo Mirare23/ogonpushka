ffc13827b595b0754e8c69ebda44f183571095c3
Tora Wijaya <72961939+Mirare23@users.noreply.github.com>
2024-01-28 01:56:30 +0800

    Build(btn): Adding Component

---
+++ components/btn.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="flex gap-10 py-5 text-2xl text-black rounded-full px-7 w-fit bg-emerald"
+  >
+    Pre-Order now
+    <icon name="uis:arrow-up-right" size="40px" />
+  </button>
+</template>
