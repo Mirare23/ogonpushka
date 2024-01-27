e7f68798bf608397d4c5a8e402669013d705c6a2
Tora Wijaya <72961939+Mirare23@users.noreply.github.com>
2024-01-28 01:56:50 +0800

    Build(navbar):Adding Component

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,14 +1,14 @@
 <template>
-  <header class="">
-    <nav class="flex justify-between px-4 py-10 text-white bg-transparent">
-      <div class="text-5xl">MYGRID</div>
+  <header class="font-sans">
+    <nav class="flex justify-between px-4 text-white bg-transparent py-7">
+      <div class="text-5xl font-semibold">MYGRID</div>
       <ul class="flex items-center gap-10">
         <li class="text-2xl">
           EN
           <icon name="ion:arrow-down-b" />
         </li>
         <li>
-          <button class="p-4 border-2 border-black rounded-full">
+          <button class="p-4 border-2 border-white rounded-full">
             <icon name="solar:hamburger-menu-outline" size="40px" />
           </button>
         </li>
