b7224235a8e9285533e997fb763917339af4db0b
Tora Wijaya <72961939+Mirare23@users.noreply.github.com>
2024-01-27 18:38:04 +0800

    Build(Navbar): Adding new navbar

---
+++ components/navBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="">
+    <nav class="flex justify-between px-4 py-10 text-white bg-transparent">
+      <div class="text-5xl">MYGRID</div>
+      <ul class="flex items-center gap-10">
+        <li class="text-2xl">
+          EN
+          <icon name="ion:arrow-down-b" />
+        </li>
+        <li>
+          <button class="p-4 border-2 border-black rounded-full">
+            <icon name="solar:hamburger-menu-outline" size="40px" />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
